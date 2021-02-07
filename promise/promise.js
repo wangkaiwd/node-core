9924bd161ccd14e934b30d441d7d48007cb16ba2
wangkaiwd <1162486176@qq.com>
2021-02-07 16:07:18 +0800

    feat: try understand then chain call

--- promise/promise.js
+++ promise/promise.js
@@ -9,43 +9,69 @@ class MyPromise {
     this.reason = undefined;
     this.resolvedFns = [];
     this.rejectedFns = [];
-    const resolve = (value) => {
-      if (this.state === PENDING) {
-        this.state = RESOLVED;
-        this.value = value;
-        this.resolvedFns.forEach(r => r());
-      }
-    };
-    const reject = (reason) => {
-      if (this.state === PENDING) {
-        this.state = REJECTED;
-        this.reason = reason;
-        this.rejectedFns.forEach(r => r());
-      }
-    };
     // 将函数传给executor,来让用户控制执行的时机
-    executor(resolve, reject);
+    try {
+      executor(this.resolve, this.reject);
+    } catch (e) {
+      this.reject(e);
+    }
   }
 
+  resolve = (value) => {
+    if (this.state === PENDING) {
+      this.state = RESOLVED;
+      this.value = value;
+      this.resolvedFns.forEach(r => r());
+    }
+  };
+  reject = (reason) => {
+    if (this.state === PENDING) {
+      this.state = REJECTED;
+      this.reason = reason;
+      this.rejectedFns.forEach(r => r());
+    }
+  };
+
+  // .then执行后会返回一个新的Promise
   then (onFulfilled, onRejected) {
-    if (this.state === RESOLVED) {
-      setTimeout(() => {
-        onFulfilled(this.value);
-      });
-    }
-    if (this.state === REJECTED) {
-      setTimeout(() => {
-        onRejected(this.reason);
-      });
-    }
-    if (this.state === PENDING) { // 等resolve或reject执行后才能确定状态
-      this.resolvedFns.push(() => {
-        onFulfilled(this.value);
-      });
-      this.rejectedFns.push(() => {
-        onRejected(this.reason);
-      });
-    }
+    return new Promise((resolve, reject) => {
+      if (this.state === RESOLVED) {
+        setTimeout(() => {
+          try {
+            // 如果当前Promise then 执行成功，并且没有明确的返回一个失败的Promise，那么它的返回值执行下一个Promise的resolve方法
+            const r = onFulfilled(this.value);
+          } catch (e) { // executor 或者 .then中传入的回调执行出错，Promise变为失败态
+            // 如果当前的Promise then onFulfilled方法执行出错了，就会执行下一个Promise的reject方法
+            reject(e);
+          }
+        });
+      }
+      if (this.state === REJECTED) {
+        setTimeout(() => {
+          try {
+            const r = onRejected(this.reason);
+          } catch (e) {
+            reject(e);
+          }
+        });
+      }
+      if (this.state === PENDING) { // 等resolve或reject执行后才能确定状态
+        this.resolvedFns.push(() => {
+          try {
+            const r = onFulfilled(this.value);
+          } catch (e) {
+            reject(e);
+          }
+        });
+        this.rejectedFns.push(() => {
+          try {
+            const r = onRejected(this.reason);
+          } catch (e) {
+            reject(e);
+          }
+        });
+      }
+    });
   }
 }
 
--- promise/test.js
+++ promise/test.js
@@ -37,4 +37,20 @@ function test2 () {
   console.log(3);
 }
 
-test2();
+// test2();
+
+function test4 () {
+  const p = new MyPromise((resolve, reject) => {
+    resolve('ok'); // 1会作为value，被then的一个函数作为参数接收
+  });
+  // 先将成功和失败回调将会被收集到Promise内部的数组中
+  p.then((val1) => {
+    return val1;
+  }, (reason) => {
+    console.log(reason);
+  }).then((val2) => {
+    console.log('val2', val2);
+  });
+  // 链式调用
+  // .then 返回一个新的Promise,onFulfilled和onRejected会在这个新的Promise1的executor中执行
+}
