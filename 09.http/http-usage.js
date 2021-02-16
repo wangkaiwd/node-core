338ef5f12360f6503b0e0719d5ebf60717bbdb9e
wangkaiwd <1162486176@qq.com>
2021-02-16 18:48:45 +0800

    feat: port has been occupied add a new listen and port plus one

--- 09.http/http-usage.js
+++ 09.http/http-usage.js
@@ -1,5 +1,5 @@
 const http = require('http');
-const PORT = 3000;
+let PORT = 3000;
 const server = http.createServer((req, res) => {
   // 将当于将该函数作为回调函数添加给request事件，当 request 事件 emit 时执行
   console.log('req,res', req, res);
@@ -16,4 +16,17 @@ const server = http.createServer((req, res) => {
 // https://devdocs.io/node~14_lts/net#net_server_listen
 server.listen(PORT, () => {
   console.log(`server is listening on port ${PORT}`);
+});
+// https://devdocs.io/node~14_lts/net#net_class_net_server: 继承自net.Server的error事件
+server.on('error', (e) => {
+  if (e.code === 'EADDRINUSE') {
+    // 端口+1重新开启一个服务
+    PORT++;
+    // server.listen(PORT, () => { // 这个会放到listening对应存放回调函数的数组中，并且由于之后加入，所以会在前一个listen事件的callback之后执行
+    //   console.log('hh');
+    // });
+    server.listen(PORT);
+    // 将会emit listening事件，执行server.listen中传入的回调，server.on('listening,cb)
+    // 在次监听时不用再添加会回调，listening 事件emit时会执行所有附加到listening上的事件
+  }
 });
--- 09.http/readme.md
+++ 09.http/readme.md
@@ -17,4 +17,5 @@
   * server.on('request',(req,res) => {})
 * [server.listen](https://devdocs.io/node~14_lts/net#net_server_listen_port_host_backlog_callback):
   如果端口被占用，可以新开一个端口再创建一个服务
+* [event error](https://devdocs.io/node~14_lts/net#net_class_net_server): 继承自net.Server的error事件
 * http.request: 可以做中间层
