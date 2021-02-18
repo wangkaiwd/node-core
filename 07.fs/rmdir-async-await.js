fea4bd346264f5f36ef51fa7bd4dd25fe2b0a02c
wangkaiwd <1162486176@qq.com>
2021-02-18 16:55:16 +0800

    feat: for of to execute in serial

--- 07.fs/rmdir-async-await.js
+++ 07.fs/rmdir-async-await.js
@@ -14,6 +14,26 @@ async function rmdir (dir) { // 删除传入的目录，并且返回promise
   }
 }
 
-rmdir(path.resolve(__dirname, 'a')).then(() => {
+async function rmdir1 (dir) {
+  const stats = await fs.stat(dir);
+  if (stats.isFile()) {
+    return await fs.unlink(dir);
+  } else { // 目录
+    const files = await fs.readdir(dir);
+    // 并行删除: 不能用Array.prototype.forEach方法
+    // https://stackoverflow.com/a/37576787/12819402
+    // await 要和for ... of 一起使用
+    for (const file of files) {
+      await rmdir1(path.join(dir, file));
+      console.log('delete', file);
+    }
+    console.log('delete after');
+    return await fs.rmdir(dir);
+  }
+}
+
+// a/b/c/d/e.js f.js
+rmdir1(path.resolve(__dirname, 'a')).then(() => {
   console.log('remove successful');
 });
+
