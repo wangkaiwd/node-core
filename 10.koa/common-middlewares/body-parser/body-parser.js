3dd9a3cabc801fda7c9a4c9a4560dd95109b7581
wangkaiwd <1162486176@qq.com>
2021-03-07 18:18:20 +0800

    feat: implement Buffer split method

--- 10.koa/common-middlewares/body-parser/body-parser.js
+++ 10.koa/common-middlewares/body-parser/body-parser.js
@@ -8,6 +8,10 @@ function bodyParser () {
   };
 }
 
+function parseMultipartFormData () {
+
+}
+
 function getBody (ctx) {
   return new Promise((resolve, reject) => {
     const arr = [];
@@ -27,7 +31,9 @@ function getBody (ctx) {
       } else if (contentType.includes('plain/text')) {
         resolve(body);
       } else if (contentType.startsWith('multipart/form-data')) { // 文件上传：二进制，可能是图片或文本
-        console.log(body);
+        // 处理服务端传来的二进制内容
+        const boundary = '--' + contentType.split('=')[1];
+        // buffer.split(boundary);
         resolve();
       }
       resolve();
@@ -36,3 +42,24 @@ function getBody (ctx) {
 }
 
 module.exports = bodyParser;
+
+// 通过一段字符串来分割buffer
+// 1111&111&11; 一段一段放到数组中
+Buffer.prototype.split = function (separator) {
+  const result = [];
+  let offset = 0;
+  let i = this.indexOf(separator, offset);
+  // this是buffer
+  // indexOf 获取分隔符的索引
+  while (i !== -1) {
+    const item = this.slice(offset, i);
+    offset = i + separator.length;
+    result.push(item);
+    i = this.indexOf(separator, offset);
+  }
+  const lastItem = this.slice(offset);
+  if (lastItem.length > 0) {
+    result.push(lastItem);
+  }
+  return result;
+};
