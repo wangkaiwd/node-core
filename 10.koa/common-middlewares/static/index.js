92d53d97e65dac6cd6f907dbcf2de0e47a788e6c
wangkaiwd <1162486176@qq.com>
2021-03-07 16:40:49 +0800

    feat: implement koa static middleware

--- 10.koa/common-middlewares/static/index.js
+++ 10.koa/common-middlewares/static/index.js
@@ -1,13 +1,25 @@
 const Koa = require('koa');
 const bodyParser = require('../body-parser/body-parser');
+const serve = require('./static');
 const app = new Koa();
 const PORT = 3000;
 
 // koa-static: 处理完静态文件后，不再执行next，那么之后的中间件便不再执行
+app.use(serve());
 app.use(bodyParser()); // 每次请求之前都会将请求内容放到ctx.request.body上
 // get /login: login page
+app.use(async (ctx, next) => {
+  if (ctx.path === '/') {
+    ctx.body = 'hello world!';
+  } else {
+    await next();
+  }
+});
+
 app.use((ctx) => {
-  ctx.body = 'hello world!';
+  if (ctx.path === '/login' && ctx.method === 'POST') {
+    ctx.body = ctx.request.body;
+  }
 });
 
 app.listen(PORT, () => {
---
+++ 10.koa/common-middlewares/static/static.js
@@ -0,0 +1,28 @@
+const path = require('path');
+const fs = require('fs/promises');
+const mime = require('mime');
+const { createReadStream, existsSync } = require('fs');
+
+function serve (dirname = '.') {
+  // 返回一个async函数
+  return async (ctx, next) => {
+    const absPath = path.join(__dirname, dirname, ctx.path);
+    const exist = existsSync(absPath);
+    if (exist) {
+      const stats = await fs.stat(absPath);
+      console.log(absPath);
+      if (stats.isFile()) {
+        const type = mime.getType(absPath);
+        ctx.set('Content-Type', `${type}; charset=utf-8`);
+        // koa will check type of ctx.body. koa will pipe it to writable stream if it is instance of Stream
+        ctx.body = createReadStream(absPath);
+      } else {
+        await next();
+      }
+    } else {
+      await next();
+    }
+  };
+}
+
+module.exports = serve;
